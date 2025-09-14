72beb45b2d2abc82734bb4f36d1b41298a20eaae
MADHU-HUB-ALT <mathumitha8645@gmail.com>
2025-09-14 13:18:22 +0530

    begginer level voice assistant

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pyttsx3
 def say(text):
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[0].id)  # Choose voice, 0 for default male voice
+    engine.setProperty('voice', voices[0].id) 
     engine.say(text)
     engine.runAndWait()
 
